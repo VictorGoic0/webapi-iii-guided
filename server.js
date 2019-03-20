1a26350a732a925eebfa84d5a8caeeca06299082
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-20 14:03:40 -0400

    guided demo complete

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
+const helmet = require("helmet");
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
 
-server.use('/api/hubs', hubsRouter);
+server.use("/api/hubs", restricted, hubsRouter);
 
-server.get('/', (req, res, next) => {
+function restricted(req, res, next) {
+  if (req.headers.token === "mellon") {
+    next();
+  } else {
+    res.status(400).send("You shall not pass!");
+  }
+}
+
+server.get("/", restricted, (req, res, next) => {
   res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome to the Lambda Hubs API</p>
